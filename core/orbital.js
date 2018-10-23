f758717c714c2785d86095f29d46b3f03a27b87a
Schahriar SaffarShargh <info@schahriar.com>
2018-10-22 21:32:32 -0700

    Adding basic loader

---
+++ core/orbital.js
@@ -0,0 +1,26 @@
+import './loaders/script';
+
+/**
+ * Orbital global definition
+ * @version 0.1.0
+ * @namespace orbital
+ */
+const orbital = {
+  loaders: new Map(),
+  /**
+   * @param {string} contentType - Content Type based on https://www.ietf.org/rfc/rfc4329.txt
+   * @param {function} handle - Loader function that registers module to orbital
+  */
+  registerLoader: (contentType, handle) => {
+    /** @todo: Throw if a loader is already registered */
+    orbital.loaders.set(contentType, handle);
+  },
+
+  import: (path) => {
+    /** @todo: Module definition */
+    /** @todo: Fetch request and fallbacks */
+    /** @todo: Content Type detection */
+  }
+};
+
+window.orbital = orbital;
